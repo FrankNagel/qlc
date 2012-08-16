e69cb48554b705fa59d5dc40d25b904c1a9942e2
Peter Bouda <pbouda@cidles.eu>
2012-08-16 15:47:21 +0000

    Added new server mirror script in Python.

--- src/webapp/quanthistling/mirror_updateservercopy.py
+++ src/webapp/quanthistling/mirror_updateservercopy.py
@@ -8,11 +8,11 @@ myzip = ZipFile("/var/www/quanthistling-new/quanthistling/public/downloads/pgdum
 myzip.extractall("/var/www/quanthistling-new/quanthistling/public/downloads/")
 myzip.close()
 
-r = re.compile("postgres")
+r = re.compile("pbouda")
 in_file = codecs.open("/var/www/quanthistling-new/quanthistling/public/downloads/pgdump_quanthistling.sql", "r", "utf-8")
 out_file = codecs.open("/var/www/quanthistling-new/quanthistling/public/downloads/pgdump_quanthistling2.sql", "w", "utf-8")
 for line in in_file:
-    new_line = r.sub("pbouda", line)
+    new_line = r.sub("postgres", line)
     out_file.write(new_line)
 in_file.close()
 out_file.close()
