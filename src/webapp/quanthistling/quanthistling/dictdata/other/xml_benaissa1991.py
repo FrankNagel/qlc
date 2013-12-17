33966e05deacf9b8825a5bc8821abbee90946c3d
jad.gaspar <>
2013-12-17 10:38:27 +0000

    Ticket#416

--- src/webapp/quanthistling/quanthistling/dictdata/other/xml_benaissa1991.py
+++ src/webapp/quanthistling/quanthistling/dictdata/other/xml_benaissa1991.py
@@ -15,11 +15,11 @@ f2 = open(sys.argv[2], 'w')
 for l in f1:
     l = l.decode('utf-8')
 
-    if not re.match(r'<p>\[(?:Seite|Spalte) \d+\]</p>', l) and not re.match(u'<p><b>\w\w? - \w\w?</b></p>', l):
+    if not re.match(r'<p>\[(?:Seite|Spalte) \d+\]</p>', l) and not re.match(u'<p><b>\w\w?, \w\w?</b></p>', l):
         l = re.sub(r'<p>(?!\t)<b>', '<p><mainentry/><b>', l)
         l = re.sub(r'<p>\t<b>', '<p><subentry/><b>\t\t', l)
 
-    if not re.match(u'<p><b>\w\w? - \w\w?</b></p>', l):
+    if not re.match(u'<p><b>\w\w?, \w\w?</b></p>', l):
         f2.write(l.encode('utf-8'))
 
 f1.close()
