10d0e59c9441c841654227d4e7fa7719fce80f02
Frank Nagel <frank.nagel@staff.uni-marburg.de>
2014-11-12 14:22:36 +0000

    Address trac555. - Split heads at '/' - Remove dashes from heads

--- src/webapp/quanthistling/scripts/annotations/annotations_for_salzerchapman1998.py
+++ src/webapp/quanthistling/scripts/annotations/annotations_for_salzerchapman1998.py
@@ -4,6 +4,8 @@ import sys
 import os
 sys.path.append(os.path.abspath('.'))
 
+import re
+
 # Pylons model init sequence
 import pylons.test
 
@@ -17,6 +19,9 @@ from paste.deploy import appconfig
 import functions
 
 
+head_end_pattern = re.compile(r'[[(]')
+head_translate = dict( (ord(c), None) for c in '-–')
+
 def annotate_head_and_pos(entry):
     # delete head annotations
     head_annotations = [a for a in entry.annotations if a.value == 'head' or
@@ -26,20 +31,8 @@ def annotate_head_and_pos(entry):
     for a in head_annotations:
         Session.delete(a)
 
-    head = None
     heads = []
-
     head_start = 0
-
-    chars_to_exclude = [u'\'', u'-', u'–']
-    chars_to_skip = 0
-    for c in entry.fullentry[:5]:
-        if c in chars_to_exclude:
-            chars_to_skip += 1
-        else:
-            break
-
-    head_start += chars_to_skip
 
     first_italic = functions.get_first_italic_range(entry)
     if first_italic != -1:
@@ -47,27 +40,30 @@ def annotate_head_and_pos(entry):
     else:
         head_end = functions.get_last_bold_pos_at_start(entry)
 
-    head = functions.insert_head(entry, head_start, head_end)
-
-    if head is not None:
-        heads.append(head)
+    for h_start, h_end in functions.split_entry_at(entry, r'/|$', head_start, head_end):
+        match = head_end_pattern.search(entry.fullentry, h_start, h_end)
+        if match:
+            h_end = match.start()
+        h_start, h_end  = functions.strip(entry, h_start, h_end, u'\'-–!? ')
+        head = entry.fullentry[h_start:h_end].translate(head_translate)
+        if head:
+            functions.insert_head(entry, h_start, h_end, head)
+            heads.append(head)
 
     # add pos
     if first_italic != -1:
-        entry.append_annotation(first_italic[0], first_italic[1], u'pos',
-                                u'dictinterpretation')
+        entry.append_annotation(first_italic[0], first_italic[1], u'pos', u'dictinterpretation')
+        head_end = first_italic[1]
 
-    return heads
+    return heads, head_end
 
 
-def annotate_translation(entry):
+def annotate_translation(entry, trans_start):
     # delete head annotations
     head_annotations = [a for a in entry.annotations if a.value == 'translation']
 
     for a in head_annotations:
         Session.delete(a)
-
-    trans_start = functions.get_pos_or_head_end(entry)
 
     trans_end = len(entry.fullentry)
     newlines = functions.get_list_ranges_for_annotation(entry, 'newline')
@@ -77,6 +73,7 @@ def annotate_translation(entry):
     brackets = functions.find_brackets(entry)
     brackets.extend(functions.find_brackets(entry, '[', ']'))
     brackets.sort()
+    brackets = functions.get_in_brackets_func(entry, brackets)
     for t_start, t_end in functions.split_entry_at(entry, '[;,] |$', trans_start, trans_end, False, brackets):
         functions.insert_translation(entry, t_start, t_end)
 
@@ -84,16 +81,16 @@ def annotate_translation(entry):
 def main(argv):
 
     bibtex_key = u'salzerchapman1998'
-    
+
     if len(argv) < 2:
         print "call: annotations_for_%s.py ini_file" % bibtex_key
         exit(1)
 
-    ini_file = argv[1]    
+    ini_file = argv[1]
     conf = appconfig('config:' + ini_file, relative_to='.')
     if not pylons.test.pylonsapp:
         load_environment(conf.global_conf, conf.local_conf)
-    
+
     # Create the tables if they don't already exist
     metadata.create_all(bind=Session.bind)
 
@@ -107,17 +104,17 @@ def main(argv):
         # entries = Session.query(model.Entry).filter_by(dictdata_id=dictdata.id,startpage=45,pos_on_page=2).all()
 
         startletters = set()
-    
+
         for e in entries:
             # print "current page: %i, pos_on_page: %i" % (e.startpage, e.pos_on_page)
-            heads = annotate_head_and_pos(e)
+            heads, start = annotate_head_and_pos(e)
+            annotate_translation(e, start)
             if not e.is_subentry:
                 for h in heads:
                     if len(h) > 0:
                         startletters.add(h[0].lower())
-            annotate_translation(e)
-        
-        dictdata.startletters = unicode(repr(sorted(list(startletters))))
+
+        dictdata.startletters = unicode(repr(sorted(startletters)))
 
         Session.commit()
 
